8b7cf5c8c0ff422d2cb79409126ec29f5d75279e
gabrielasantiago <gabrielasantiago@gabrielas-mbp.cpe.libertypr.com>
2017-11-15 18:34:23 -0400

    Centered the frame.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,9 +2,9 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myName = new JFrame("Hello, world!");
+		JFrame myName = new JFrame("Gabriela Santiago ");
 		myName.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myName.setLocation(600, 300);
+		myName.setLocation(600, 350);
 		myName.setSize(200, 200);
 		myName.setVisible(true);
 	}
